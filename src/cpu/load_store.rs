6d5bab4b17eaffd3bfea95d298a29ae6e1fd8cb4
Francesco Boffa <fra.boffa@gmail.com>
2025-05-18 20:47:42 +0200

    Add load and store operations

---
+++ src/cpu/load_store.rs
@@ -0,0 +1,108 @@
+use super::{Cpu, Instruction};
+
+impl Cpu {
+    /// 20 - LB - I-type
+    /// LB rt, offset(rs)
+    /// GPR[rt] = sign_extend(Memory[rs + offset, 8-bit])
+    pub(super) fn ins_lb(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.ram.read(address, 1) as i8 as u32;
+
+        self.write_reg(instr.rt(), value);
+    }
+
+    /// 21 - LH - I-type
+    /// LH rt, offset(rs)
+    /// GPR[rt] = sign_extend(Memory[rs + offset, 16-bit])
+    pub(super) fn ins_lh(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.ram.read(address, 2) as i16 as u32;
+
+        self.write_reg(instr.rt(), value);
+    }
+
+    /// 23 - LW - I-type
+    /// LW rt, offset(rs)
+    /// GPR[rt] = Memory[rs + offset, 32-bit]
+    pub(super) fn ins_lw(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.ram.read(address, 4);
+
+        self.write_reg(instr.rt(), value);
+    }
+
+    /// 24 - LBU - I-type
+    /// LBU rt, offset(rs)
+    /// GPR[rt] = zero_extend(Memory[rs + offset, 8-bit])
+    pub(super) fn ins_lbu(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.ram.read(address, 1) as u32;
+
+        self.write_reg(instr.rt(), value);
+    }
+
+    /// 25 - LHU - I-type
+    /// LHU rt, offset(rs)
+    /// GPR[rt] = zero_extend(Memory[rs + offset, 16-bit])
+    pub(super) fn ins_lhu(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.ram.read(address, 2) as u32;
+
+        self.write_reg(instr.rt(), value);
+    }
+
+    /// 28 - SB - I-type
+    /// SB rt, offset(rs)
+    /// Memory[rs + sign_extended(offset), 8-bit] = GPR[rt]
+    pub(super) fn ins_sb(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.get_rt(instr);
+
+        self.ram.write(address, value, 1);
+    }
+
+    /// 29 - SH - I-type
+    /// SH rt, offset(rs)
+    /// Memory[rs + sign_extended(offset), 16-bit] = GPR[rt]
+    pub(super) fn ins_sh(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.get_rt(instr);
+
+        self.ram.write(address, value, 2);
+    }
+
+    /// 2B - SW - I-type
+    /// SW rt, offset(rs)
+    /// Memory[rs + sign_extended(offset), 32-bit] = GPR[rt]
+    pub(super) fn ins_sw(&mut self, instr: Instruction) {
+        let address = self.target_address(instr);
+        let value = self.get_rt(instr);
+
+        self.ram.write(address, value, 4);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cpu::test_utils::*;
+
+    #[test]
+    fn test_sw() {
+        let mut cpu = test_cpu(
+            &[(7, 0x1000), (8, 0x12345678)],
+            &[
+                // SW r8, 0(r7)
+                i_type(0x2b, 8, 7, 0),
+                // SW r8, -4(r7)
+                i_type(0x2b, 8, 7, 0xfffc),
+            ],
+        );
+
+        cpu_steps(&mut cpu, 2);
+        assert_eq!(cpu.read_memory(0x1000, 4).unwrap(), 0x12345678);
+        assert_eq!(cpu.read_memory(0x0ffc, 4).unwrap(), 0x12345678);
+
+        // Test that the store was done in little-endian order
+        assert_eq!(cpu.read_memory(0x1000, 2).unwrap(), 0x5678);
+    }
+}
