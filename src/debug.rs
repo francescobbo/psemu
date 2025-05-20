32f29df7bca0fa0afb1ec52917a481ce7ca94df6
Francesco Boffa <fra.boffa@gmail.com>
2025-05-20 10:59:37 +0200

    Multiplication

--- src/debug.rs
+++ src/debug.rs
@@ -45,7 +45,11 @@ impl Debugger {
     pub fn enter(&mut self, cpu: &mut Cpu) -> bool {
         // Present the current instruction
         let ins = cpu.read_memory(cpu.pc, AccessSize::Word).unwrap();
-        println!("[{:08x}]    {}", cpu.pc, Self::disassemble(Instruction(ins)));
+        println!(
+            "[{:08x}]    {}",
+            cpu.pc,
+            Self::disassemble(Instruction(ins))
+        );
 
         loop {
             // Read a command from the user
@@ -134,7 +138,10 @@ impl Debugger {
         println!("   pc -> {:08x}", cpu.pc);
 
         if let Some(load_delay) = &cpu.load_delay {
-            println!("Pending load: {} -> {:08x}", REGISTERS[load_delay.target], load_delay.value);
+            println!(
+                "Pending load: {} -> {:08x}",
+                REGISTERS[load_delay.target], load_delay.value
+            );
         }
     }
 
