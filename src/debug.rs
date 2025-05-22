090220e40499c2d58d8e973dad15b6199858f600
Francesco Boffa <fra.boffa@gmail.com>
2025-05-22 22:26:58 +0200

    Add register names to the debugger

--- src/debug.rs
+++ src/debug.rs
@@ -2,18 +2,25 @@ use crate::{cpu::Cpu, ram::AccessSize};
 
 pub struct Debugger {}
 
+const REGISTERS: [&str; 32] = [
+    "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
+    "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
+    "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
+    "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra",
+];
+
 impl Debugger {
     /// Prints the contents of the registers
     pub fn print_registers(cpu: &Cpu) {
         for (i, &value) in cpu.registers.iter().enumerate() {
-            print!("r{i:<2}: {value:08x}  ");
+            print!("{:>5} -> {value:08x}  ", REGISTERS[i]);
 
             if i % 4 == 3 {
                 println!();
             }
         }
 
-        println!("pc: {:#08x}", cpu.pc);
+        println!("   pc -> {:08x}", cpu.pc);
     }
 
     /// Prints the contents of a memory location, as a little endian 32-bit
