7321f5c16b8f1f0cf67ae0a3589ffe1d99b14f41
Francesco Boffa <fra.boffa@gmail.com>
2025-06-06 23:08:04 +0200

    [ram2] Stub implementation of accessor methods

--- src/ram.rs
+++ src/ram.rs
@@ -13,5 +13,31 @@ impl Ram {
             data: vec![0; RAM_SIZE],
         }
     }
+
+    //[ ram-accessors
+    fn read8(&self, address: u32) -> u8 {
+        self.data[address as usize]
+    }
+
+    fn read16(&self, address: u32) -> u16 {
+        todo!("Implement read16");
+    }
+
+    fn read32(&self, address: u32) -> u32 {
+        todo!("Implement read32");
+    }
+
+    fn write8(&mut self, address: u32, value: u8) {
+        self.data[address as usize] = value;
+    }
+
+    fn write16(&mut self, address: u32, value: u16) {
+        todo!("Implement write16");
+    }
+
+    fn write32(&mut self, address: u32, value: u32) {
+        todo!("Implement write32");
+    }
+    //] ram-accessors
 }
 //] ram-new
