ea9832165dcbc9fcb4e6f6c89e9b6703eece1116
Francesco Boffa <fra.boffa@gmail.com>
2025-06-07 19:08:40 +0200

    [cpu2] A basic cpu cycle

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -25,5 +25,44 @@ impl Cpu {
             ram: Ram::new(),
         }
     }
+
+    //[ cpu-stubs
+    pub fn step(&mut self) {
+        // Fetch the instruction at the current program counter (PC).
+        let instruction = self.fetch_instruction(self.pc).unwrap();
+
+        // Update the program counter to point to the next instruction.
+        self.pc += 4;
+
+        // Execute the instruction based on its opcode and function code.
+        self.execute(instruction);
+    }
+
+    fn fetch_instruction(&self, address: u32) -> Result<u32, ()> {
+        self.read_memory(address, AccessSize::Word)
+    }
+
+    fn execute(&mut self, instruction: u32) {
+        println!("Executing instruction: {instruction:x}");
+    }
+
+    pub fn read_memory(
+        &self,
+        address: u32,
+        size: AccessSize,
+    ) -> Result<u32, ()> {
+        Ok(self.ram.read(address, size))
+    }
+
+    pub fn write_memory(
+        &mut self,
+        address: u32,
+        value: u32,
+        size: AccessSize,
+    ) -> Result<(), ()> {
+        self.ram.write(address, value, size);
+        Ok(())
+    }
+    //] cpu-stubs
 }
 //] cpu-new
