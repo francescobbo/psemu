6529ac931f34966d9293bcacf60a1d1f064de637
Francesco Boffa <fra.boffa@gmail.com>
2025-06-08 19:26:53 +0200

    [log5] Implement the stubbed logic instructions

--- src/cpu/logic.rs
+++ src/cpu/logic.rs
@@ -7,14 +7,20 @@ impl Cpu {
     /// SLTI rt, rs, immediate
     /// GPR[rt] = (signed(GPR[rs]) < sign_extend(immediate)) ? 1 : 0
     pub fn ins_slti(&mut self, instruction: Instruction) {
-        // Your implementation here
+        let value = instruction.simm16();
+        let result = ((self.get_rs(instruction) as i32) < value) as u32;
+
+        self.write_reg(instruction.rt(), result)
     }
 
     /// 0B - SLTIU - I-type
     /// SLTIU rt, rs, immediate
     /// GPR[rt] = (GPR[rs] < unsigned(sign_extend(immediate))) ? 1 : 0
     pub fn ins_sltiu(&mut self, instruction: Instruction) {
-        // Your implementation here
+        let value = instruction.simm16() as u32;
+        let result = (self.get_rs(instruction) < value) as u32;
+
+        self.write_reg(instruction.rt(), result)
     }
     //] slti-stubs
     //[ logic-instructions
@@ -22,31 +28,31 @@ impl Cpu {
     /// ANDI rt, rs, immediate
     /// GPR[rt] = GPR[rs] & immediate
     pub(super) fn ins_andi(&mut self, instr: Instruction) {
-        // Your implementation here
+        self.write_reg(instr.rt(), self.get_rs(instr) & instr.imm16());
     }
 
     /// 0D - ORI - I-Type
     /// ORI rt, rs, immediate
     /// GPR[rt] = GPR[rs] | immediate
     pub(super) fn ins_ori(&mut self, instr: Instruction) {
-        // Your implementation here
+        self.write_reg(instr.rt(), self.get_rs(instr) | instr.imm16());
     }
 
     /// 0E - XORI - I-Type
     /// XORI rt, rs, immediate
     /// GPR[rt] = GPR[rs] ^ immediate
     pub(super) fn ins_xori(&mut self, instr: Instruction) {
-        // Your implementation here
+        self.write_reg(instr.rt(), self.get_rs(instr) ^ instr.imm16());
     }
     //] logic-instructions
-    //[ logic-lui
+    //[ ins-lui
     /// 0F - LUI - I-Type
     /// LUI rt, immediate
     /// GPR[rt] = immediate << 16
     pub(super) fn ins_lui(&mut self, instr: Instruction) {
-        // Your implementation here
+        self.write_reg(instr.rt(), instr.imm16() << 16);
     }
-    //] logic-lui
+    //] ins-lui
 }
 //] new-logic
 //[ !omit
