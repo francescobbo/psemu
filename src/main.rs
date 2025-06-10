95ec59e9a667e80e3521898f407ab1150b73e4ff
Francesco Boffa <fra.boffa@gmail.com>
2025-06-10 15:23:36 +0200

    [ls4] Test load/add/store program

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,21 @@ fn main() {
     let mut cpu = Cpu::new();
 
     // Write a test program to memory 
-    cpu.write_memory(0, 0x2401_0002, AccessSize::Word);
-    cpu.write_memory(4, 0x2421_0003, AccessSize::Word);
+    cpu.write_memory(0, 0x2402_00fc, AccessSize::Word);
+    cpu.write_memory(4, 0x8c43_0004, AccessSize::Word);
+    cpu.write_memory(8, 0x2463_0001, AccessSize::Word);
+    cpu.write_memory(12, 0xac43_0008, AccessSize::Word);
+
+    // Write a value at 0x100
+    cpu.write_memory(0x100, 41, AccessSize::Word);
 
     // Execute the program
-    for _ in 0..2 {
+    for _ in 0..4 {
         cpu.step();
     }
 
-    println!("r1: {}", cpu.registers[1]);
+    // Read the result from memory, at 0x104
+    let result = cpu.read_memory(0x104, AccessSize::Word).unwrap();
+    println!("Result: {}", result);
 }
 //] main
